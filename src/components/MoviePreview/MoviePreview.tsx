1969c4968312179ed4ed0ef7e78948faa7f0c6cf
Yasmine Thabet <yasminethabet4@gmail.com>
2024-05-29 20:27:58 +0200

    Movie Card

--- src/components/MoviePreview/MoviePreview.tsx
+++ src/components/MoviePreview/MoviePreview.tsx
@@ -1,11 +1,24 @@
+import { POSTER_URL_PREFIX } from "../../App";
+import { Movie } from "../../models";
 import styles from "./MoviePreview.module.css";
 
-const MoviePreview = () => {
+const MoviePreview = ({movie}:{movie:Movie}) => {
   const goToMovieDetails = () => {
     // A DEFINIR EXPERT
   };
+  console.log(movie.poster_path)
 
-  return <div className={styles.movieCard} onClick={goToMovieDetails}></div>;
+  return <div className={styles.movieCard} onClick={goToMovieDetails}>
+    <img src={POSTER_URL_PREFIX+movie.poster_path} crossOrigin=""></img>
+    <span>{movie.title}</span>
+    <div id="stars">
+      {[...Array(5)].map((_, index) => (
+        <span className={`${styles.quizzItem} ${  
+          (index<(movie.vote_average/2)) ? styles.yellow : styles.white
+        }`}>★</span>
+      ))}
+    </div>
+  </div>;
 };
 
 export default MoviePreview;
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -9,7 +9,7 @@ type TMDBResponse<T> = {
 };
 
 const BASE_URL_API = "https://api.themoviedb.org/3";
-const API_KEY = "xxx";
+const API_KEY = "d60b9236200ea914f0975a476ecdbd79";
 
 const DEFAULT_SEARCH_PARAMS = {
   api_key: API_KEY,
